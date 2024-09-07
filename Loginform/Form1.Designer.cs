df53ebaf80c7fd453a42b88406d520873834da6c
Cuartero-AdrianBenedict-BSCS21A1 <cuartero.adrian13@gmail.com>
2024-09-08 01:23:46 +0800

    Add project files.

---
+++ Loginform/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿using System;
+
+namespace Loginform
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.password = new System.Windows.Forms.Panel();
+            this.Username = new System.Windows.Forms.Label();
+            this.pw = new System.Windows.Forms.Label();
+            this.enter = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HP Simplified Jpan", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Agent authorization log in";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(172, 377);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 10);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.Red;
+            this.password.Location = new System.Drawing.Point(172, 504);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(631, 10);
+            this.password.TabIndex = 3;
+            this.password.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("HP Simplified Jpan", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(92, 289);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(195, 35);
+            this.Username.TabIndex = 5;
+            this.Username.Text = "Agent username";
+            this.Username.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pw
+            // 
+            this.pw.AutoSize = true;
+            this.pw.Font = new System.Drawing.Font("HP Simplified Jpan", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pw.Location = new System.Drawing.Point(92, 418);
+            this.pw.Name = "pw";
+            this.pw.Size = new System.Drawing.Size(193, 35);
+            this.pw.TabIndex = 7;
+            this.pw.Text = "Agent Password";
+            this.pw.Click += new System.EventHandler(this.password_Click);
+            // 
+            // enter
+            // 
+            this.enter.BackColor = System.Drawing.Color.Red;
+            this.enter.Font = new System.Drawing.Font("HP Simplified Jpan", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.enter.Location = new System.Drawing.Point(192, 545);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(189, 73);
+            this.enter.TabIndex = 8;
+            this.enter.Text = "ENTER";
+            this.enter.UseVisualStyleBackColor = false;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Red;
+            this.exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.exit.Font = new System.Drawing.Font("HP Simplified Jpan", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.exit.Location = new System.Drawing.Point(516, 545);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(189, 73);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "EXIT";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("HP Simplified Jpan", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtUserName.Location = new System.Drawing.Point(172, 331);
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(628, 40);
+            this.txtUserName.TabIndex = 10;
+            this.txtUserName.Text = "login username";
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Font = new System.Drawing.Font("HP Simplified Jpan", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtpassword.Location = new System.Drawing.Point(172, 458);
+            this.txtpassword.Multiline = true;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(628, 40);
+            this.txtpassword.TabIndex = 10;
+            this.txtpassword.Text = "login password";
+            this.txtpassword.TextChanged += new System.EventHandler(this.txtpassword_TextChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Loginform.Properties.Resources._51d6def892f689d44a7ab1db6b0907151;
+            this.pictureBox3.Location = new System.Drawing.Point(100, 452);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(66, 62);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Loginform.Properties.Resources._51d6def892f689d44a7ab1db6b0907151;
+            this.pictureBox2.Location = new System.Drawing.Point(100, 325);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(66, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Loginform.Properties.Resources.valorant_logo_freelogovectors_net_;
+            this.pictureBox1.Location = new System.Drawing.Point(192, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.pw);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "login";
+            this.Text = "System Log in";
+            this.TransparencyKey = System.Drawing.Color.IndianRed;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Panel password;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label pw;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private EventHandler exit_Click;
+    }
+}
+
---
+++ Loginform/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Loginform
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserManager
+    {
+        private List<User> users;
+        private string filePath = "C:\\Users\\ghoul\\source\\repos\\Loginform\\Loginform\\users.json";
+
+        public UserManager()
+        {
+            LoadUsersFromJson();
+        }
+        private void LoadUsersFromJson()
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                users = JsonConvert.DeserializeObject<List<User>>(json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading user data: " + ex.Message);
+                users = new List<User>(); // Ensure users is not null
+            }
+        }
+        public bool IsValidUser(string username, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    public partial class login : Form
+    {
+        private UserManager userManager;
+
+        public login()
+        {
+            InitializeComponent();
+            userManager = new UserManager();
+        }
+
+        private void enter_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtpassword.Text;
+
+            if (userManager.IsValidUser(username, password))
+            {
+                MessageBox.Show("Login successful!");
+                new login().Show(); // Show new form
+                this.Hide(); // Hide the current form
+            }
+            else
+            {
+                MessageBox.Show("User input invalid! Try again.");
+                txtUserName.Clear();
+                txtpassword.Clear();
+                txtUserName.Focus();
+            }
+        }
+
+        private void exit_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("System Exited");
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void pictureBox2_Click(object sender, EventArgs e) { }
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void panel1_Paint(object sender, PaintEventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void password_Click(object sender, EventArgs e) { }
+        private void txtUserName_TextChanged(object sender, EventArgs e) { }
+        private void txtpassword_TextChanged(object sender, EventArgs e) { }
+    }
+}
+
